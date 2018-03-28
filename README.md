da351e8afbcaa7193e5f2494b5f19ee6ff8c8898
pfaic <pfaic@hsr.ch>
2018-03-28 17:12:21 +0200

    löschen

--- README.md
+++
@@ -1,2 +0,0 @@
-# PinUp
-This is our EPJ Project for further information about the project don't bother to contact one of the contributors
--- controller/controller.js
+++
@@ -1,6 +0,0 @@
-const map = require('../service/map.js');
-
-module.exports.showIndex = function(req, res){
-  const title = "PinUp"
-  res.render("index.hbs", {title});
-}
