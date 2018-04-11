34646557fcdd6f4b9d1636350e466247da4f69d3
cedi95 <cedricwagner95@gmail.com>
2018-04-11 14:49:40 +0200

    Prettier Tests

--- src/test/match.test.js
+++ src/test/match.test.js
@@ -5,19 +5,18 @@ let expect = require("chai").use(require("sinon-chai")).expect;
 
 //Tests the login functionality of firebase
 describe("Test Pin creating", function() {
-
-    describe("#getmaches", function () {
-        it('should contain certain Posts', function () {
-
-        });
-        it('should contain no matching Posts', function () {
-
-        });
-        it('should contain any Post that is in within Radius', function () {
-
-        });
-        it('should lose one Post during actual update', function () {
-
-        });
+  describe("#getmaches", function() {
+    it("should contain certain Posts", function() {
+      //Content
     });
+    it("should contain no matching Posts", function() {
+      //Content
+    });
+    it("should contain any Post that is in within Radius", function() {
+      //Content
+    });
+    it("should lose one Post during actual update", function() {
+      //Content
+    });
+  });
 });
--- src/test/post.test.js
+++ src/test/post.test.js
@@ -3,36 +3,34 @@ import { shallow, mount, render } from "enzyme";
 let sinon = require("sinon");
 let expect = require("chai").use(require("sinon-chai")).expect;
 
-
 describe("Test Pin", function() {
-
-    describe("#createPost", function () {
-        it('should create valid Post', function () {
-
-        });
-        it('should request Client to fill out missing Input / Informations', function () {
-
-        });
-        it('should request Client to change Input to valid Values', function () {
-
-        });
+  describe("#createPost", function() {
+    it("should create valid Post", function() {
+      //Content
     });
-
-    describe("#changePost",function () {
-        it('should update Pin with new Information', function () {
-
-        });
-        it('should deny update and ask Client to add valid Input', function () {
-
-        });
+    it("should request Client to fill out missing Input / Informations", function() {
+      //Content
     });
-
-    describe("#deletePost",function () {
-        it('should be deleted after Request from Client', function () {
-
-        });
-        it('should get deleted after the "survival" Date', function () {
-            //Might not that good testable due to "cleaning" function which might just runs once every 24h
-        });
+    it("should request Client to change Input to valid Values", function() {
+      //Content
     });
+  });
+
+  describe("#changePost", function() {
+    it("should update Pin with new Information", function() {
+      //Content
+    });
+    it("should deny update and ask Client to add valid Input", function() {
+      //Content
+    });
+  });
+
+  describe("#deletePost", function() {
+    it("should be deleted after Request from Client", function() {
+      //Content
+    });
+    it('should get deleted after the "survival" Date', function() {
+      //Might not that good testable due to "cleaning" function which might just runs once every 24h
+    });
+  });
 });
