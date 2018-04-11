095aacec10497bf12ed6897629a4dd3cbe416094
cedi95 <cedricwagner95@gmail.com>
2018-04-11 14:24:08 +0200

    Test Framework slightly changed

---
+++ src/test/system.test.js
@@ -0,0 +1,19 @@
+import React from "react";
+import { shallow, mount, render } from "enzyme";
+let sinon = require("sinon");
+let expect = require("chai").use(require("sinon-chai")).expect;
+
+//Tests the login functionality of firebase
+/*TODO System Test
+    Creates several Posts and Pins and saves them on
+    the Firebase (most likely local copy of Firebase DB)
+    Tests the matching algorithm if it gives back the Posts
+    which matches the parameter of the Pins.
+ */
+test(true);
+/*
+describe("Test a complete run with different Elements",function () {
+    it('should work', function () {
+
+    });
+});*/
