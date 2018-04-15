212ca1dd7e4f1b215a2e96824cbcc12de1efd85b
Tiziano Gemma <tiziano@gemma-mail.ch>
2018-04-15 10:53:28 +0200

    created Event.js

---
+++ src/business/Event.js
@@ -0,0 +1,6 @@
+import * as dbRef from '../constants/dbRef'
+import {db} from '../datalayer/firebase/firebase'
+import {onOwn} from './Helper'
+import * as GeoFire from 'geofire'
+
+//TODO implement: create, take part, ...
