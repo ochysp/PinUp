1e145f1258edaa880161c40ebbd7b0606f80c707
Tiziano Gemma <tiziano@gemma-mail.ch>
2018-04-20 10:02:03 +0200

    Created PostDetails.js

---
+++ src/components/Post/PostDetails.js
@@ -0,0 +1,16 @@
+// @flow
+
+import React from 'react';
+import type { PinType } from '../../business/Types';
+
+type State = {
+};
+
+type Props = {
+  pinData: PinType
+};
+
+export default class PinDetails extends React.Component<Props, State> {
+
+
+}
