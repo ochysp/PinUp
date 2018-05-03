1148c96c62a73b66b9806054169bb740a5a45b86
Unknown <cedricwagner95@gmail.com>
2018-05-03 11:14:05 +0200

    Preparation for tests

--- src/test/system.test.js
+++ src/test/system.test.js
@@ -1,8 +1,8 @@
-// TODO: Anpassung an eslint wenn Test geschrieben werden
-/*
-eslint-disable no-unused-vars,prefer-destructuring */
+/* eslint-disable no-unused-vars,prefer-destructuring */
 import React from 'react';
+import { doCreateUser } from '../business/User';
 
+const sinon = require('sinon');
 const expect = require('chai').use(require('sinon-chai')).expect;
 const proxyquire = require('proxyquire');
 
@@ -12,15 +12,35 @@ const mockauth = new firebasemock.MockAuthentication();
 const mockdatabase = new firebasemock.MockFirebase();
 const mocksdk = new firebasemock.MockFirebaseSdk(path =>
   (path ? mockdatabase.child(path) : mockdatabase), () => mockauth);
+mocksdk.initializeApp();
 
 
-const mySrc = proxyquire('../data/firebase/firebase.js', {
-  firebase: mocksdk,
+proxyquire('../data/firebase/firebase.js', {
+  db: mockdatabase,
 });
-mockdatabase.autoFlush();
+mockdatabase.autoFlush(true);
+const authUser = {
+  uid: '123',
+  displayName: 'Max Muster',
+  email: 'maxmuster@gmail.com',
+  photoURL: null,
+};
+const location = {
+  latitude: 47.22354,
+  longitude: 8.81714,
+};
 
 describe('Test a complete run with different Elements', () => {
-  it('should work', () => {
-
+  it('should go fuck itself', () => {
+    doCreateUser(
+      '123', 'Max Muster', 'maxmuster@gmail.com', null,
+    );
+    /*
+    expect(mockdatabase.child('userData').child('userInfo').getData()).to.equal({
+      123: {
+        email: 'maxmuster@gmail.com',
+        username: 'Max Muster',
+      },
+    }); */
   });
 });
