78fed52cb546b785db5b466fd5f2fa74ebe89f8a
Tiziano Gemma <tiziano@gemma-mail.ch>
2018-05-04 12:09:46 +0200

    removed unused styles

--- src/components/Post/ListOfPosts.js
+++ src/components/Post/ListOfPosts.js
@@ -1,35 +1,24 @@
 // @flow
 
 import React from 'react';
-import { List, withStyles } from 'material-ui';
+import { List } from 'material-ui';
 import PostListEntry from './PostListEntry';
 import type { AuthUserType, KeyType } from '../../business/Types';
 
 type Props = {
-  classes: any,
   posts: KeyType[],
   authUser: AuthUserType
 };
-
-const styles = theme => ({
-  root: {
-    width: '100%',
-    maxWidth: 360,
-    backgroundColor: theme.palette.background.paper,
-  },
-});
 
 const ListOfPosts = (props: Props) => {
   const listItems = props.posts.map(postId => (
     <PostListEntry postId={postId} authUser={props.authUser} key={postId} />
   ));
   return (
-    <div className={props.classes.root}>
-      <List component="nav">
-        {listItems}
-      </List>
-    </div>
+    <List component="nav">
+      {listItems}
+    </List>
   );
 };
 
-export default withStyles(styles)(ListOfPosts);
+export default (ListOfPosts);
