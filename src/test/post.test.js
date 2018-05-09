fe2855d002ad61187bb8d50713add9ea80db1d57
Cedric Wagner <cedricwagner95@gmail.com>
2018-05-09 10:35:11 +0200

    Working Posttest

--- src/test/post.test.js
+++ src/test/post.test.js
@@ -1,15 +1,34 @@
-/* eslint-disable no-unused-vars,prefer-destructuring */
+/* eslint-disable no-unused-vars,prefer-destructuring,flowtype/no-types-missing-file-annotation */
 import React from 'react';
 import Enzyme, { shallow } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 // setTestRun activates the Firebase TestDB. It needs to be the first of all relative imports.
 import '../data/firebase/setTestRun';
-import { CATEGORIES } from '../constants/categories';
 import CreatePostForm from '../components/Post/CreatePostForm';
 import { listenForAllPostsOfUser } from '../business/Post';
 import { deleteTestDbOnRootLevel, haltIfLiveDB } from './testHelpers';
+import { PostType } from '../business/Types';
+import { listenForAllPinsOfUser } from '../business/Pin';
 
 Enzyme.configure({ adapter: new Adapter() });
+
+const authUser = {
+  uid: '256',
+  displayName: 'Karls Kramer',
+  email: 'karlskramer@gmail.com',
+  photoURL: null,
+};
+const location = {
+  latitude: 47.22354,
+  longitude: 8.81714,
+};
+const state = { posts: [] };
+const expectedPostData = {
+  category: '0',
+  location,
+  title: 'testpost_1234',
+  userId: authUser.uid,
+};
 
 afterEach(() => {
   deleteTestDbOnRootLevel();
@@ -20,43 +39,41 @@ beforeEach(() => {
   deleteTestDbOnRootLevel();
 });
 
-describe('Test a complete run with different Elements', () => {
-  it('should render correctly', (done) => {
-    const authUser = {
-      uid: '123',
-      displayName: 'Max Muster',
-      email: 'maxmuster@gmail.com',
-      photoURL: null,
-    };
-    const location = {
-      latitude: 47.22354,
-      longitude: 8.81714,
-    };
-    const postForm = shallow(<CreatePostForm authUser={authUser} position={location} />);
+const createPost = () => {
+  const root = shallow(<CreatePostForm authUser={authUser} position={location} />);
+  const postForm = root.find('CreatePostForm').dive();
 
+  postForm.setState({
+    title: 'testpost_1234',
+    category: '0',
+    invalidSubmit: false,
+    sentToDB: false,
+    dialogIsActive: true,
+  });
+  const button = postForm.find('[id="Save"]');
+  button.simulate('click');
+  return postForm;
+};
 
-    function callback(data) {
-      expect(data).toBe('peanut butter jelly time');
-      done();
+const doAfterPostCreation = (toDo) => {
+  const postForm = createPost();
+  function callback(data) {
+    if (data.length && data.length > 0) {
+      toDo(postForm, data);
     }
+  }
+  listenForAllPostsOfUser(authUser.uid, callback);
+};
 
-    const pf = postForm.find('CreatePostForm').dive();
-    pf.setState({
-      title: 'testpost1234',
-      category: '0',
-      invalidSubmit: false,
-      sentToDB: false,
-      dialogIsActive: true,
-    });
-    const state1 = pf.state;
-    const button = pf.find('[id="Save"]');
-    button.simulate('click');
-
-    const state2 = pf.state;
-
-    console.log(state2);
-    // console.log(postForm.debug());
-    const x = 0;
+describe('Test a complete run with different Elements', () => {
+  it('create valid Post', (done) => {
+    const checkData = (postForm, data) => {
+      // eslint-disable-next-line no-param-reassign
+      delete data[0].postId;
+      expect(data[0]).toEqual(expectedPostData);
+      done();
+    };
+    doAfterPostCreation(checkData);
   });
   it('', () => {
   });
